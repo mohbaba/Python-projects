01b386268ae76018e8b94faefe42bcc403cc25c7
mohbaba <babamu09@gmail.com>
2022-11-21 08:42:59 +0100

    First Commit

---
+++ python_calculator.py
@@ -0,0 +1,11 @@
+def add(a,b):
+    result = a + b
+    return result 
+
+def subtract(a,b):
+    result = a - b
+    return result
+
+def multiply(a,b,c=1,d=1,e=1):
+    result = a * b
+    return result 
